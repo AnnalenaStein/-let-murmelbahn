374d4b949c5de2cbc70693dca8398633407955e1
vanihlrr <vanessa.hiller@hfg.design>
2024-01-10 09:55:14 +0100

    Endless geupdated

--- js/ps_step_endless.js
+++ js/ps_step_endless.js
@@ -14,11 +14,12 @@ let isDrag = false;
 // an array to contain all the blocks created
 let blocks = [];
 let murmel;
+let angle = 0;
 
 let canvasElem;
 let off = { x: 0, y: 0 };
 
- // das ist die Dimension des kompletten Levels 
+ // das ist die Dimension des kompletten Levels
  const dim = { w: 3840, h: 2160 };
 
 function preload() {
@@ -26,6 +27,7 @@ function preload() {
 }
 
 function setup() {
+  document.getElementsByClassName('overlay')[0].style.display = 'none';
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('thecanvas');
 
@@ -35,8 +37,31 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
 
+    // the ball has a label and can react on collisions
+    murmel = new Ball(world,
+      { x: 300, y: 100, r: 25, color: 'green' },
+      { label: "Murmel", density: 0.004, restitution: 0.5, friction: 0.0, frictionAir: 0.0 }
+    );
+    blocks.push(murmel);
+
+      // add a mouse so that we can manipulate Matter objects
+  mouse = new Mouse(engine, canvas, { stroke: 'blue', strokeWeight: 3 });
+
    Runner.run(engine);
   }
+
+  blocks.push(new BlockCore(
+    world,
+    {
+      x: 150, y: 250, w: 70, h: 10, color: 'red',
+      trigger: (ball, blocks) => { //Trigger für Musik
+        console.log("Trigger", ball, blocks); 
+        // mySound.play(); //Klavier Sound wird abgespielt 
+        E_xyl.play();
+      }
+    },
+    { isStatic: true }
+  ));
 
  function scrollEndless(point) {
   // wohin muss verschoben werden damit point wenn möglich in der Mitte bleibt
