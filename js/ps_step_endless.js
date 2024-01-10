3266d67f4dd10df94de10deff6bc2f188d382778
vanihlrr <vanessa.hiller@hfg.design>
2024-01-10 10:19:16 +0100

    Endless Scroll FUNKTIONIERT!!!!

--- js/ps_step_endless.js
+++ js/ps_step_endless.js
@@ -16,6 +16,8 @@ let blocks = [];
 let murmel;
 let angle = 0;
 
+let jazz_bg;
+
 let canvasElem;
 let off = { x: 0, y: 0 };
 
@@ -39,29 +41,27 @@ function setup() {
 
     // the ball has a label and can react on collisions
     murmel = new Ball(world,
-      { x: 300, y: 100, r: 25, color: 'green' },
+      { x: 300, y: 100, r: 25, color: 'white' },
       { label: "Murmel", density: 0.004, restitution: 0.5, friction: 0.0, frictionAir: 0.0 }
     );
     blocks.push(murmel);
 
       // add a mouse so that we can manipulate Matter objects
-  mouse = new Mouse(engine, canvas, { stroke: 'blue', strokeWeight: 3 });
+   mouse = new Mouse(engine, canvas, { stroke: 'blue', strokeWeight: 3 });
 
    Runner.run(engine);
-  }
-
-  blocks.push(new BlockCore(
-    world,
-    {
-      x: 150, y: 250, w: 70, h: 10, color: 'red',
-      trigger: (ball, blocks) => { //Trigger für Musik
-        console.log("Trigger", ball, blocks); 
-        // mySound.play(); //Klavier Sound wird abgespielt 
-        E_xyl.play();
+   
+   blocks.push(new BlockCore(
+     world,
+     {
+       x: 150, y: 250, w: 2000, h: 10, color: 'grey',
+       trigger: (ball, blocks) => { //Trigger für Musik
+        //console.log("Trigger", ball, blocks); 
       }
     },
     { isStatic: true }
-  ));
+    ));
+  }
 
  function scrollEndless(point) {
   // wohin muss verschoben werden damit point wenn möglich in der Mitte bleibt
@@ -77,9 +77,23 @@ function setup() {
   mouse.setOffset(off);
 }
 
+function keyPressed(event) {
+  switch (keyCode) {
+    case 32:
+      console.log("Space");
+      event.preventDefault();
+      Matter.Body.applyForce(murmel.body, murmel.body.position, { x: 0.2, y: -0.2   });
+      // Matter. Body.scale(murmel.body, 1.5, 1.5);
+      break;
+    default:
+      console.log(keyCode);
+  }
+}
+
 
 function draw() {
   background('blue');
+
   clear();
 
   // position canvas and translate coordinates
