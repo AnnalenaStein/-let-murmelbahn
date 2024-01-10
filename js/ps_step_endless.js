acb6fbfa8965ee2c4b832b40b28cefea65d5f9ba
vanihlrr <vanessa.hiller@hfg.design>
2024-01-10 12:00:59 +0100

    Note eingefügt in endless datei

--- js/ps_step_endless.js
+++ js/ps_step_endless.js
@@ -17,6 +17,7 @@ let murmel;
 let angle = 0;
 
 let jazz_bg;
+let noteImg;
 
 let canvasElem;
 let off = { x: 0, y: 0 };
@@ -26,6 +27,7 @@ let off = { x: 0, y: 0 };
 
 function preload() {
   jazz_bg = loadImage('./img/jazz-bg.png');
+  noteImg = loadImage('./img/note.png');
 }
 
 function setup() {
@@ -41,10 +43,18 @@ function setup() {
 
     // the ball has a label and can react on collisions
     murmel = new Ball(world,
-      { x: 50, y: 100, r: 25, color: 'white' },
+      { x: 50, y: 60, r: 25, fromFile: './img/Note.svg', image: noteImg  },
       { label: "Murmel", density: 0.005, restitution: 0.2, friction: 0.001, frictionAir: 0.007 } //Murmel wird bei frictionAir ein bisschen gebremst
     );
     blocks.push(murmel);
+
+      //add ball with notenImage
+  //NOTE 
+  // murmel = new Ball(
+  //   world,
+  //   { x: 50, y: 60, fromFile: './img/note.svg', image: noteImg },
+  //   { label: "Murmel", density: 0.005, restitution: 0.2, friction: 0.001, frictionAir: 0.007 }
+  // ); blocks.push(murmel);
 
       // add a mouse so that we can manipulate Matter objects
    mouse = new Mouse(engine, canvas, { stroke: 'blue', strokeWeight: 3 });
@@ -54,7 +64,7 @@ function setup() {
    blocks.push(new BlockCore(
      world,
      {
-       x: 150, y: 250, w: 300, h: 20, color: 'grey', 
+       x: 150, y: 250, w: 300, h: 20, color: 'orange', 
        trigger: (ball, blocks) => { //Trigger für Musik
         //console.log("Trigger", ball, blocks); 
       }
@@ -65,7 +75,7 @@ function setup() {
    blocks.push(new BlockCore(
     world,
     {
-      x: 550, y: 250, w: 300, h: 20, color: 'grey',
+      x: 550, y: 250, w: 300, h: 20, color: 'orange',
       trigger: (ball, blocks) => { //Trigger für Musik
        //console.log("Trigger", ball, blocks); 
      }
@@ -76,7 +86,7 @@ function setup() {
    blocks.push(new BlockCore(
     world,
     {
-      x: 1050, y: 220, w: 500, h: 20, color: 'grey',
+      x: 1050, y: 220, w: 500, h: 20, color: 'orange',
       trigger: (ball, blocks) => { //Trigger für Musik
        //console.log("Trigger", ball, blocks); 
      }
@@ -87,7 +97,7 @@ function setup() {
    blocks.push(new BlockCore(
     world,
     {
-      x: 1650, y: 200, w: 320, h: 20, color: 'grey',
+      x: 1650, y: 200, w: 320, h: 20, color: 'orange',
       trigger: (ball, blocks) => { //Trigger für Musik
        //console.log("Trigger", ball, blocks); 
      }
@@ -99,7 +109,7 @@ function setup() {
    blocks.push(new BlockCore(
     world,
     {
-      x: 100, y: 400, w: 2500, h: 20, color: 'grey',
+      x: 100, y: 1000, w: 2500, h: 20, color: 0,
       trigger: (ball, blocks) => { //Trigger für Musik
        //console.log("Trigger", ball, blocks); 
      }
@@ -111,7 +121,7 @@ function setup() {
    blocks.push(new BlockCore(
     world,
     {
-      x: 1900, y: 300, w: 20, h: 500, color: 'grey',
+      x: 1900, y: 300, w: 20, h: 500, color: 0,
       trigger: (ball, blocks) => { //Trigger für Musik
        //console.log("Trigger", ball, blocks); 
      }
@@ -122,7 +132,7 @@ function setup() {
    blocks.push(new BlockCore(
     world,
     {
-      x: 1750, y: 500, w: 320, h: 20, color: 'red',
+      x: 1750, y: 500, w: 320, h: 20, color: 'orange',
       trigger: (ball, blocks) => { //Trigger für Musik
        //console.log("Trigger", ball, blocks); 
      }
