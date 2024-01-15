b1f1685966178ba9329011baf6e91ba2335bffb6
JasminaKramer <jasmina.kramer@hfg.design>
2024-01-15 15:54:03 +0100

    langer canvas hinugefügt

---
+++ js/ps_step_endlesslang.js
@@ -0,0 +1,185 @@
+Homeworks.aufgabe = 7;
+
+const Engine = Matter.Engine;
+const Runner = Matter.Runner;
+const Bodies = Matter.Bodies;
+const Events = Matter.Events;
+const World = Matter.World;
+
+// the Matter engine to animate the world
+let engine;
+let world;
+let mouse;
+let isDrag = false;
+// an array to contain all the blocks created
+let blocks = [];
+let murmel;
+let angle = 0;
+
+let jazz_bg;
+let noteImg;
+
+let canvasElem;
+let off = { x: 0, y: 0 };
+
+ // das ist die Dimension des kompletten Levels
+ const dim = { w: 8160, h: 1080 };
+
+function preload() {
+  jazz_bg = loadImage('./img/jazz-bg.png');
+  noteImg = loadImage('./img/note.png');
+}
+
+function setup() {
+  document.getElementsByClassName('overlay')[0].style.display = 'none';
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent('thecanvas');
+
+  // Das ist nötig für den 'Endless Canvas'
+  canvasElem = document.getElementById('thecanvas');
+
+  engine = Engine.create();
+  world = engine.world;
+
+    // the ball has a label and can react on collisions
+    murmel = new Ball(world,
+      { x: 50, y: 60, r: 25, fromFile: './img/Note.svg', image: noteImg  },
+      { label: "Murmel", density: 0.005, restitution: 0.2, friction: 0.001, frictionAir: 0.007 } //Murmel wird bei frictionAir ein bisschen gebremst
+    );
+    blocks.push(murmel);
+
+      //add ball with notenImage
+  //NOTE 
+  // murmel = new Ball(
+  //   world,
+  //   { x: 50, y: 60, fromFile: './img/note.svg', image: noteImg },
+  //   { label: "Murmel", density: 0.005, restitution: 0.2, friction: 0.001, frictionAir: 0.007 }
+  // ); blocks.push(murmel);
+
+      // add a mouse so that we can manipulate Matter objects
+   mouse = new Mouse(engine, canvas, { stroke: 'blue', strokeWeight: 3 });
+
+   Runner.run(engine);
+   
+   blocks.push(new BlockCore(
+     world,
+     {
+       x: 150, y: 250, w: 300, h: 20, color: 'orange', 
+       trigger: (ball, blocks) => { //Trigger für Musik
+        //console.log("Trigger", ball, blocks); 
+      }
+    },
+    { isStatic: true }
+    ));
+
+   blocks.push(new BlockCore(
+    world,
+    {
+      x: 550, y: 250, w: 300, h: 20, color: 'orange',
+      trigger: (ball, blocks) => { //Trigger für Musik
+       //console.log("Trigger", ball, blocks); 
+     }
+   },
+   { isStatic: true }
+   ));
+
+   blocks.push(new BlockCore(
+    world,
+    {
+      x: 1050, y: 220, w: 500, h: 20, color: 'orange',
+      trigger: (ball, blocks) => { //Trigger für Musik
+       //console.log("Trigger", ball, blocks); 
+     }
+   },
+   { isStatic: true }
+   ));
+
+   blocks.push(new BlockCore(
+    world,
+    {
+      x: 1650, y: 200, w: 320, h: 20, color: 'orange',
+      trigger: (ball, blocks) => { //Trigger für Musik
+       //console.log("Trigger", ball, blocks); 
+     }
+   },
+   { isStatic: true }
+   ));
+
+   //Unterer Balken
+   blocks.push(new BlockCore(
+    world,
+    {
+      x: 100, y: 1000, w: 2500, h: 20, color: 0,
+      trigger: (ball, blocks) => { //Trigger für Musik
+       //console.log("Trigger", ball, blocks); 
+     }
+   },
+   { isStatic: true }
+   ));
+
+   // Verikaler Balken
+  //blocks.push(new BlockCore(
+   // world,
+  //  {
+   //   x: 1900, y: 300, w: 20, h: 500, color: 0,
+  //    trigger: (ball, blocks) => { //Trigger für Musik
+       //console.log("Trigger", ball, blocks); 
+   //  }
+  // },
+  // { isStatic: true }
+  // ));
+
+   blocks.push(new BlockCore(
+    world,
+    {
+      x: 1750, y: 500, w: 320, h: 20, color: 'orange',
+      trigger: (ball, blocks) => { //Trigger für Musik
+       //console.log("Trigger", ball, blocks); 
+     }
+   },
+   { isStatic: true }
+   ));
+ }
+  
+
+ function scrollEndless(point) {
+  // wohin muss verschoben werden damit point wenn möglich in der Mitte bleibt
+  off = { x: Math.min(Math.max(0, point.x - windowWidth / 2), dim.w -  windowWidth), y: Math.min(Math.max(0, point.y - windowHeight / 2), dim.h -  windowHeight) };
+  // plaziert den Canvas im aktuellen Viewport
+  canvasElem.style.left = Math.round(off.x) + 'px';
+  canvasElem.style.top = Math.round(off.y) + 'px';
+  // korrigiert die Koordinaten
+  translate(-off.x, -off.y);
+  // verschiebt den ganzen Viewport
+  window.scrollTo(off.x, off.y);
+  // Matter mouse needs the offset as well
+  mouse.setOffset(off);
+}
+
+function keyPressed(event) {
+  switch (keyCode) {
+    case 32:
+      console.log("Space");
+      event.preventDefault();
+      Matter.Body.applyForce(murmel.body, murmel.body.position, { x: 0.18, y: -0.18  });
+      // Matter. Body.scale(murmel.body, 1.5, 1.5);
+      break;
+    default:
+      console.log(keyCode);
+  }
+}
+
+
+function draw() {
+  background('blue');
+
+  clear();
+
+  // position canvas and translate coordinates
+  scrollEndless(murmel.body.position);
+
+  // animate attracted blocks
+  blocks.forEach(block => block.draw());
+  mouse.draw();
+}
+
