831ac0e53c264c3104edd9689124145ed53f4101
vanihlrr <vanessa.hiller@hfg.design>
2024-01-22 23:07:30 +0100

    Breite und Höhe angepasst :)

--- js/ps_step_murmelbahn.js
+++ js/ps_step_murmelbahn.js
@@ -36,7 +36,7 @@ let canvasElem;
 let off = { x: 0, y: 0 };
 
  // das ist die Dimension des kompletten Levels
- const dim = { w: 2000, h: 16000 };
+ const dim = { w: 1800, h: 16800 };
 
 function preload() {
   music_bg = loadImage('./img/music_lines_bg.png')
